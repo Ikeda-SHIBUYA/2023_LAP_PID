c2a61b71229b564d360e471731314aed1f37e0b1
Ikeda-SHIBUYA <140130722+Ikeda-SHIBUYA@users.noreply.github.com>
2024-07-26 15:14:58 +0900

    OBJ upload

---
+++ OBJ/module_cfg.h
@@ -0,0 +1,9 @@
+#pragma once
+extern ID _module_id_APP_INIT_TASK;
+#define APP_INIT_TASK ((const ID)(_module_id_APP_INIT_TASK))
+extern ID _module_id_MAIN_TASK;
+#define MAIN_TASK ((const ID)(_module_id_MAIN_TASK))
+extern ID _module_id_TRACER_TASK;
+#define TRACER_TASK ((const ID)(_module_id_TRACER_TASK))
+extern ID _module_id_CYC_TRACER;
+#define CYC_TRACER ((const ID)(_module_id_CYC_TRACER))
